ff4dfa6cd8ef0cc220a05c3fbae22444b85617a2
Philippe Gerum <rpm@xenomai.org>
2017-03-07 17:38:16 +0100

    stop_machine: ipipe: ensure atomic stop-context operations
    stop_machine() guarantees that all online CPUs are spinning
    non-preemptible in a known code location before a subset of them may
    safely run a stop-context function. This service is typically useful
    for live patching the kernel code, or changing global memory mappings,
    so that no activity could run in parallel until the system has
    returned to a stable state after all stop-context operations have
    completed.
    
    When interrupt pipelining is enabled, we have to provide the same
    guarantee by restoring hard interrupt disabling where virtualizing the
    interrupt disable flag would defeat it.

--- kernel/stop_machine.c
+++ kernel/stop_machine.c
@@ -225,6 +225,7 @@ static int multi_cpu_stop(void *data)
 		}
 	} while (curstate != MULTI_STOP_EXIT);
 
+	hard_irq_enable();
 	local_irq_restore(flags);
 	return err;
 }
@@ -591,6 +592,7 @@ int stop_machine_cpuslocked(cpu_stop_fn_t fn, void *data,
 		local_irq_save(flags);
 		hard_irq_disable();
 		ret = (*fn)(data);
+		hard_irq_enable();
 		local_irq_restore(flags);
 
 		return ret;
