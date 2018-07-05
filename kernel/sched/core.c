b84b94f95fd7e49629779595c9d6a9e29155b7f4
Philippe Gerum <rpm@xenomai.org>
2018-07-05 08:55:08 +0200

    sched: ipipe: announce CPU affinity change
    Emit IPIPE_KEVT_SETAFFINITY to the co-kernel when the target task is
    about to move to another CPU.
    
    CPU migration can only take place from the root domain, the pipeline
    does not provide any support for migrating tasks from the head domain,
    and derives several key assumptions based on this invariant.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1765,7 +1765,6 @@ static int __set_cpus_allowed_ptr(struct task_struct *p,
 	}
 
 	__ipipe_report_setaffinity(p, dest_cpu);
-
 	if (task_running(rq, p) || p->state == TASK_WAKING) {
 		struct migration_arg arg = { p, dest_cpu };
 		/* Need help from migration thread: drop lock and wait. */
