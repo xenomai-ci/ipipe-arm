b29ff3cb1f6c489f4fff2bff17dc6ace3427c48e
Greg Gallagher <greg@embeddedgreg.com>
2021-04-17 01:22:29 -0400

    arm: ipipe: Bump release tag
    Signed-off-by: Greg Gallagher <greg@embeddedgreg.com>

--- arch/arm/include/asm/ipipe.h
+++ arch/arm/include/asm/ipipe.h
@@ -44,7 +44,7 @@ extern unsigned long arm_return_addr(int level);
 #include <linux/jump_label.h>
 #include <linux/ipipe_trace.h>
 
-#define IPIPE_CORE_RELEASE	13
+#define IPIPE_CORE_RELEASE	14
 
 struct ipipe_domain;
 struct timekeeper;
